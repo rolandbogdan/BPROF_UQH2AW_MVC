06c2ff516a0199cc52c3d49276cd090f2378a65e
Bogdán Roland <bogdanroland07@gmail.com>
2020-11-23 23:12:36 +0100

    Added controllers, views. Created the index file, works so far

---
+++ PcWebapp/PcWebapp/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Logic;
+
+namespace PcWebapp.Controllers
+{
+    public class HomeController : Controller
+    {
+        CustomerLogic customerlogic;
+        ProductLogic productlogic;
+        OrderLogic orderlogic;
+
+        public HomeController(CustomerLogic customerlogic, ProductLogic productlogic, OrderLogic orderlogic)
+        {
+            this.customerlogic = customerlogic;
+            this.productlogic = productlogic;
+            this.orderlogic = orderlogic;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
