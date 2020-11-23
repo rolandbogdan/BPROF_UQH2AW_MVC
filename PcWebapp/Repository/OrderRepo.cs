d2065de50c52ca0163156943440c03e8c9b4afbe
Bogdán Roland <bogdanroland07@gmail.com>
2020-11-23 22:43:58 +0100

    Orders repo created

---
+++ PcWebapp/Repository/OrderRepo.cs
@@ -0,0 +1,60 @@
+﻿using Data;
+using Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Repository
+{
+    public class OrderRepo : IRepository<Order>
+    {
+        PCStoreContext ctx = new PCStoreContext();
+        public void Add(Order order)
+        {
+            ctx.Orders.Add(order);
+        }
+
+        public void Delete(Order order)
+        {
+            ctx.Orders.Remove(order);
+        }
+
+        public void Delete(string OrderID)
+        {
+            Delete(Read(OrderID));
+        }
+
+        public Order Read(string OrdID)
+        {
+            return ctx.Orders.FirstOrDefault(x => x.OrderID == OrdID);
+        }
+
+        public IQueryable<Order> Read()
+        {
+            return ctx.Orders.AsQueryable();
+        }
+
+        public void Save()
+        {
+            ctx.SaveChanges();
+        }
+
+        public void Update(string oldOrderID, Order newOrder)
+        {
+            var oldOrder = Read(oldOrderID);
+            //quantity, date, comment, status
+            oldOrder.OrderedQuantity = newOrder.OrderedQuantity;
+            oldOrder.OrderDate = newOrder.OrderDate;
+            oldOrder.Comment = newOrder.Comment;
+            oldOrder.OrderStatus = newOrder.OrderStatus;
+
+            oldOrder.Customers.Clear();
+            foreach (var item in newOrder.Customers)
+            {
+                oldOrder.Customers.Add(item);
+            }
+            Save();
+        }
+    }
+}
