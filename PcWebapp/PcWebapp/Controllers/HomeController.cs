b7d9baa9ba75f52992675a0ad15c7ad391660d7f
Bogdán Roland <bogdanroland07@gmail.com>
2020-12-08 13:38:03 +0100

    Saving products to json files added

--- PcWebapp/PcWebapp/Controllers/HomeController.cs
+++ PcWebapp/PcWebapp/Controllers/HomeController.cs
@@ -5,6 +5,8 @@ using System.Linq;
 using System.Threading.Tasks;
 using Logic;
 using Models;
+using Newtonsoft.Json;
+using System.IO;
 
 namespace PcWebapp.Controllers
 {
@@ -40,6 +42,19 @@ namespace PcWebapp.Controllers
         public IActionResult ListProducts()
         {
             return View(productlogic.GetAllProducts());
+        }
+        public IActionResult ProductDataGenerator()
+        {
+            StreamWriter sw = new StreamWriter("products.json");
+            foreach (var item in productlogic.GetAllProducts())
+            {
+                sw.Write(JsonConvert.SerializeObject(item));
+            }
+            return RedirectToAction(nameof(Index));
+        }
+        public IActionResult DataGenerator()
+        {
+            return View();
         }
     }
 }
